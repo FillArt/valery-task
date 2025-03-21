bcb9e0a4f5116e04ee8d0d4ef5aa4070f33c9682
FillArt <artiom.filipopovschii@gmail.com>
2025-03-21 19:58:02 -0300

    feat(password): added basic structure

--- src/features/pasword/PasswordInput.jsx
+++ src/features/pasword/PasswordInput.jsx
@@ -1,8 +1,14 @@
+import {useState} from "react";
 
-export const PasswordInput = () => {
+export const PasswordInput = ({value, onChange, placeholder='placeholder text...'}) => {
+
+    const [isHiddenPassword, setIsHiddenPassword] = useState(true)
+    const inputType = isHiddenPassword ? 'text' : 'password'
+
     return (
         <div>
-
+            <input type={inputType} value={value} onChange={onChange} placeholder={placeholder}/>
+            <button>(O)</button>
         </div>
     );
 };
--- src/pages/home/ui/Home.jsx
+++ src/pages/home/ui/Home.jsx
@@ -2,16 +2,19 @@ import {useState} from "react";
 
 import {Modal} from "@/features/modal/Modal.jsx";
 import {Button} from "@/shared/ui/Button.jsx";
+import {PasswordInput} from "@/features/pasword/PasswordInput.jsx";
 
 export const Home = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [password, setPassword] = useState("");
 
     return (
         <>
             <div className="d-flex vh-100 justify-content-center align-items-center">
                 {isModalOpen ? (
                     <Modal title="Modal title" onClose={() => setIsModalOpen(false)}>
-                        There will be an input here
+                        <p>Hello World!</p>
+                        <PasswordInput value={password} onChange={setPassword}/>
                     </Modal>
                 ) : (
                     <Button title="Show modal" onClick={() => setIsModalOpen(true)}/>
