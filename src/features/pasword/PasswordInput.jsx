319c940dbb9f370b5c7f6fbe2f991af935c63b8a
FillArt <artiom.filipopovschii@gmail.com>
2025-03-21 20:41:06 -0300

    feat(password): added eye icon for input. Finished logic

--- src/features/pasword/PasswordInput.jsx
+++ src/features/pasword/PasswordInput.jsx
@@ -1,17 +1,60 @@
 import {useState} from "react";
 import styled from "styled-components";
 
-export const PasswordInput = ({value, onChange, placeholder='placeholder text...'}) => {
+import EyeIcon from "./assets/eye.svg"
+import EyeIconSlash from "./assets/eye-slash.svg"
+
+
+export const PasswordInput = ({value, onChange, placeholder='enter text here...'}) => {
 
     const [isHiddenPassword, setIsHiddenPassword] = useState(true)
     const inputType = isHiddenPassword ? 'text' : 'password'
+    const eyeIcon = isHiddenPassword ? EyeIconSlash : EyeIcon
+
+    const onChangeHandler = (e) => onChange(e.currentTarget.value)
 
     return (
-        <div>
-            <input type={inputType} value={value} onChange={onChange} placeholder={placeholder}/>
-            <button onClick={() => setIsHiddenPassword(!isHiddenPassword)}>(O)</button>
-        </div>
+        <PasswordContainer>
+            <input type={inputType} value={value} onChange={onChangeHandler} placeholder={placeholder}/>
+            <button onClick={() => setIsHiddenPassword(!isHiddenPassword)}>
+                <img src={eyeIcon} alt="Toggle visibility"/>
+            </button>
+        </PasswordContainer>
     );
 };
 
-const PasswordContainer = styled.div``
+const PasswordContainer = styled.div`
+    display: flex;
+    align-items: center;
+    
+    box-sizing: border-box;
+    padding: 10px 20px;
+    border-radius: 10px;
+    border: 1px solid blue;
+
+    &:focus-within {
+        outline: 3px solid skyblue;
+    }
+    
+    input {
+        border: none;
+        flex: 1;
+        padding: 0 20px 0 0;
+        &:focus {
+            outline: none;
+        }
+    }
+    button {
+        background: none;
+        display: inherit;
+        align-items: inherit;
+        border: none;
+        width: 20px;
+        height: 20px;
+        img {
+            width: 20px;
+            height: 20px;
+        }
+    }
+`
+
