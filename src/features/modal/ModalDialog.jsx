e4c0436c4d3f75c6ac4abf494e3e5daffe429a63
FillArt <artiom.filipopovschii@gmail.com>
2025-03-21 17:39:28 -0300

    feat: added basic project structure according to FSD

---
+++ src/features/modal/ModalDialog.jsx
@@ -0,0 +1,8 @@
+
+export const ModalDialog = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
---
+++ src/features/pasword/PasswordInput.jsx
@@ -0,0 +1,8 @@
+
+export const PasswordInput = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
