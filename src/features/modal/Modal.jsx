e903c2ba41b22a3abf3897b422fbdfd0a4eaf799
FillArt <artiom.filipopovschii@gmail.com>
2025-03-21 22:39:25 -0300

    feat(modal): added closing of modal window with "ESC" key

--- src/features/modal/Modal.jsx
+++ src/features/modal/Modal.jsx
@@ -1,7 +1,20 @@
 import {ModalDialog} from "./ModalDialog.jsx";
 import {ModalBackground} from "./ModalBackground.jsx";
+import {useEffect} from "react";
 
 export const Modal = ({children, title, onClose}) => {
+
+    useEffect(() => {
+        const handleEscPress = (event) => {
+            if (event.key === "Escape") { onClose(); }
+        };
+        document.addEventListener("keydown", handleEscPress);
+
+        return () => {
+            document.removeEventListener("keydown", handleEscPress);
+        };
+    }, [onClose]);
+
     return (
         <ModalBackground onClose={onClose}>
             <ModalDialog title={title} onClose={onClose}>
