858f6fdd603afc9d2e282aa328a8149ba14610fa
FillArt <artiom.filipopovschii@gmail.com>
2025-03-21 22:02:07 -0300

    refactor: fixed transparency of modal window. Added return of password secrecy when closing modal window

--- src/features/modal/ModalBackground.jsx
+++ src/features/modal/ModalBackground.jsx
@@ -1,7 +1,7 @@
 export const ModalBackground = ({children, onClose}) => {
     return (
         <div className="modal fade show d-block" onClick={() => onClose(false)} tabIndex="-1" role="dialog">
-            <div className="modal-backdrop fade show">
+            <div style={{ background: 'rgba(0, 0, 0, 0.5)' }} className="position-fixed top-0 left-0 vw-100 vh-100 fade show ">
                 <div className="modal-dialog modal-dialog-centered" role="document">
                     {children}
                 </div>
@@ -9,3 +9,11 @@ export const ModalBackground = ({children, onClose}) => {
         </div>
     );
 };
+
+//
+// position: fixed;
+// top: 0;
+// left: 0;
+// z-index: var(--bs-backdrop-zindex);
+// width: 100vw;
+// height: 100vh;
